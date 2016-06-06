38b2ca7a6ce2247be08e2637334df6a4dd1a7fa6
parent.j.f <parent.j.f@gmail.com>
2016-06-06 13:43:00 +0000

    Added slack_api_key optinal arguments

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ def cli():
 @click.command()
 @click.argument('channel')
 @click.argument('msg')
-def send_msg(channel, msg):
-    slack_api_key = os.environ.get('SLACK_API_KEY', False)
+@click.argument('slack_api_key', required = False)
+def send_msg(channel, msg, slack_api_key = None):
     if not slack_api_key:
-        raise Exception("Set the SLACK_API_KEY env variable first")
+        slack_api_key = os.environ.get('SLACK_API_KEY', False)
+        if not slack_api_key:
+            raise Exception("Set the SLACK_API_KEY env variable first")
 
     slack = Slacker(slack_api_key)
 
