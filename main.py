8634ecdace73b058210787c8f14fc75c6b091e73
parent.j.f <parent.j.f@gmail.com>
2016-06-06 12:55:27 +0000

    Added channel

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ def cli():
     pass
 
 @click.command()
+@click.argument('channel')
 @click.argument('msg')
-def send_msg(msg):
+def send_msg(channel, msg):
     slack_api_key = os.environ.get('SLACK_API_KEY', False)
     if not slack_api_key:
         raise Exception("Set the SLACK_API_KEY env variable first")
 
     slack = Slacker(slack_api_key)
 
-    slack.chat.post_message('#general', msg)
+    slack.chat.post_message(channel, msg)
 
 cli.add_command(send_msg)
